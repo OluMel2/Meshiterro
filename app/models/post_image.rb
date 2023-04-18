813c9dc2ed1b0f416f6e8605ac0905c8cb6e81b9
OluMel2 <129649026+OluMel2@users.noreply.github.com>
2023-04-18 11:55:11 +0000

    [21章]アプリ２

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -5,6 +5,9 @@ class PostImage < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  validates :shop_name, presence: true
+  validates :image, presence: true
+
   def get_image
     if image.attached?
       image
@@ -18,8 +21,8 @@ class PostImage < ApplicationRecord
     file_path = Rails.root.join('app/assets/images/no_image.jpg')
     image.attach(io:File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
   end
-  image
-end
+   image
+  end
 
 def favorited_by?(user)
   favorites.exists?(user_id: user.id)
